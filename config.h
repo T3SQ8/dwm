4344f5fcf125161c66da6376f4363379cf96b9fe
T3SQ8 <h9s@tutanota.com>
2022-12-24 03:43:38 +0100

    Aesthetic changes

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -11,13 +11,13 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int splitstatus        = 1;        /* 1 for split status items */
 static const char *splitdelim       = ";";       /* Character used for separating status */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "JetBrains Mono:size=10", "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static const char *fonts[]          = { "JetBrains Mono:size=10", "Font Awesome 6 Pro:size=9" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#3E7440";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -25,7 +25,9 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+static const char *tags[] = { "", "", "", "", "", "", "", "", "", ""};
+//static const char *tags[] = { "", "", "", "", "", "", "", "", "", ""};
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
 
 static const Rule rules[] = {
 	/* xprop(1):
